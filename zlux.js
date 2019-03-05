0b333c9e369f86afcf56b2c8ec2a7f6a9ba33e09
zulfikra <zulfikralahmudin@gmail.com>
2019-03-05 16:27:50 +0700

    add new zlux js

--- zlux.js
+++ zlux.js
@@ -1,15 +1,17 @@
 var argv = require('yargs').argv
 var fs   = require('fs')
-
-function database(name)
+function database(name,schema)
 {
+    if(schema == null) schema = 'public'
     return `
 const { database } = require('../core/database.js')
 const  argv = require('yargs').argv
 switch(argv.migration)
 {
     case "create":
-        database.CreateTable('${name}')
+        database
+        ${(schema != null) ? `.CreateSchema('${schema}')` : ''}
+        .CreateTable('${schema}','${name}')
                 .Fields('${name}_id')
                     .PrimaryKey()
                     .NotNull()
@@ -17,21 +19,19 @@ switch(argv.migration)
                 .Execute()
         break;
     case "drop":
-        database.DropTable('${name}')
+        database.DropTable('${schema}','${name}')
                 .Execute()
         break;
         case "force-drop":
-    database.DropTableCascade('users_manajemen','${name}')
+    database.DropTableCascade('${schema}','${name}')
         .Execute()
         break;
 }
     `
 }
-
-
 switch(argv.make){
     case "table" :
-        fs.writeFile(`${__dirname}/database/${argv.name}.js`,database(argv.name),(err) => {
+        fs.writeFile(`${__dirname}/database/${argv.name}.js`,database(argv.name,(argv.schema != undefined)?argv.schema : null),(err) => {
             if(err) {
                 console.log(err);
                 return
